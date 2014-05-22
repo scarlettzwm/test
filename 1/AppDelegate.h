5035b5edb26acff3b906ac508b88d35ae95a8ba5
scarlettzwm <scarlettzwm@gmail.com>
2014-05-22 19:26:24 +0800

    rr
    rr

---
+++ 1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by zhouwm on 14-5-22.
+//  Copyright (c) 2014年 zhouwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
